c373c79a2212cb0bc70e6c841c98689f7b8646dc
b0hne <sebastian.benkel@tum.de>
2020-08-12 01:56:43 +0200

    adds camera_config path and switches to usb_camera_driver_node

--- launch/pi_car.launch.py
+++ launch/pi_car.launch.py
@@ -2,21 +2,27 @@ import launch
 import launch.actions
 import launch.substitutions
 from launch_ros.actions import Node
-
+ns = 'pi_car'
 def generate_launch_description():
     return launch.LaunchDescription([
         Node(
             package='pi_car',
-            namespace='pi_car',
-            executable='pi_car',
-            name='pi_car'
+            namespace=ns,
+            executable='pi_car'
         ),
 
         Node(
-            package='image_tools',
-            namespace='pi_car',
-            executable='cam2image',
-            name='cam2image'
+            package='usb_camera_driver',
+
+            # package='image_tools',
+            # executable='cam2image',
+            namespace=ns, 
+            executable='usb_camera_driver_node',
+            parameters=[{
+                'camera_calibration_file': 'file:///home/ubuntu/.ros/camera_info/camera.yaml',
+            }
+        ]
+
         )
 
 
